f8ae3a4ec03d0d8f1e9d74aa4d01984c3582b5db
Tamas Menyhart <tamas.menyhart@supercharge.io>
2021-01-22 15:21:02 +0100

    feat: create theme variations

---
+++ src/theme/DefaultDark.theme.ts
@@ -0,0 +1,22 @@
+import { ColorTheme, SpacingTheme, Theme } from './Theme.interface';
+
+const DEFAULT_DARK_COLOR_THEME: ColorTheme = {
+  primary: '#03a9f4',
+  onPrimary: '#fff',
+  surface: '#545454',
+  onSurface: '#fff',
+  background: '#3f3f3f',
+};
+
+const DEFAULT_DARK_SPACING_THEME: SpacingTheme = {
+  base: 8,
+  double: 16,
+};
+
+export const DEFAULT_DARK_THEME_ID = 'default-dark';
+
+export const DEFAULT_DARK_THEME: Theme = {
+  id: DEFAULT_DARK_THEME_ID,
+  color: DEFAULT_DARK_COLOR_THEME,
+  spacing: DEFAULT_DARK_SPACING_THEME,
+};
---
+++ src/theme/DefaultLight.theme.ts
@@ -0,0 +1,22 @@
+import { ColorTheme, SpacingTheme, Theme } from './Theme.interface';
+
+const DEFAULT_LIGHT_COLOR_THEME: ColorTheme = {
+  primary: '#03a9f4',
+  onPrimary: '#fff',
+  surface: '#fff',
+  onSurface: '#000',
+  background: '#dedede',
+};
+
+const DEFAULT_LIGHT_SPACING_THEME: SpacingTheme = {
+  base: 8,
+  double: 16,
+};
+
+export const DEFAULT_LIGHT_THEME_ID = 'default-light';
+
+export const DEFAULT_LIGHT_THEME: Theme = {
+  id: DEFAULT_LIGHT_THEME_ID,
+  color: DEFAULT_LIGHT_COLOR_THEME,
+  spacing: DEFAULT_LIGHT_SPACING_THEME,
+};
