e4af79d163b20aec88ca75b59717aac88eabf522
Tamas Menyhart <tamas.menyhart@supercharge.io>
2021-01-22 16:28:19 +0100

    feat: render the ScreenView component

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
 
 import { DEFAULT_LIGHT_THEME, ThemeProvider } from './theme';
+import { ScreenView } from './ScreenView.component';
 import { Toolbar } from './Toolbar.component';
-
-const Styles = StyleSheet.create({
-  root: {
-    flex: 1,
-  },
-});
 
 export const App = React.memo(() => {
   return (
-    <SafeAreaView>
-      <ThemeProvider initial={DEFAULT_LIGHT_THEME}>
-        <View style={Styles.root}>
-          <Toolbar title={'Hello Reactive Styles!'} />
-        </View>
-      </ThemeProvider>
-    </SafeAreaView>
+    <ThemeProvider initial={DEFAULT_LIGHT_THEME}>
+      <ScreenView>
+        <Toolbar title={'Hello Reactive Styles!'} />
+      </ScreenView>
+    </ThemeProvider>
   );
 });
