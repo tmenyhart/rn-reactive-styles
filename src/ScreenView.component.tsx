7bd1c39a3b1637e741f23f99e2a68ac1df0edfe7
Tamas Menyhart <tamas.menyhart@supercharge.io>
2021-01-22 16:27:36 +0100

    feat: create the ScreenView component

---
+++ src/ScreenView.component.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Theme, useThemeAwareObject } from './theme';
+
+const createStyles = (theme: Theme) => {
+  const styles = StyleSheet.create({
+    root: {
+      backgroundColor: theme.color.background,
+      flex: 1,
+    },
+  });
+  return styles;
+};
+
+export interface ScreenViewProps {
+  children?: React.ReactNode;
+}
+
+export const ScreenView = React.memo<ScreenViewProps>((props) => {
+  const Styles = useThemeAwareObject(createStyles);
+
+  return <View style={Styles.root}>{props.children}</View>;
+});
