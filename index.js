3d0ef50e9426f1c2db52ab6fc38429eaefbe941c
Tamas Menyhart <tamas.menyhart@supercharge.io>
2021-01-22 15:02:20 +0100

    refactor: update the App component

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
-import App from './src/App.component';
+import { App } from './src/App.component';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
